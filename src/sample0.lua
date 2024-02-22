e965a83f762d8e1555a6f886ca06175f319c3a97
OveraLL <ru111kravtsov@gmail.com>
2024-02-22 20:59:27 +0200

    added new gitignore

--- src/sample0.lua
+++ src/sample0.lua
@@ -1,77 +1,80 @@
 function CastToDecimal(str, ctx)
-    local decimal = str:gsub('%.+', ''):match('%d+')
-    return ctx:CreateDecimal(decimal) or nil
+    local decimal = str:gsub('%,+', ''):match('%d+%.%d%d')
+    return decimal and ctx:CreateDecimal(decimal) or nil
 end
 function PostExtract(fields, tables, ctx)
-    if fields._InvoiceTotal then
-        fields.InvoiceTotal = CastToDecimal(fields._InvoiceTotal.Text, ctx)
-    end
-    if fields._Anchor0 and fields.InvoiceNumber then
+    local total
+    local description
+    if fields._Anchor2 then
         local text = ctx['text']
         text = text:GetArea(
-                fields._Anchor0.BBox.Right,
-                fields.InvoiceNumber.BBox.Bottom,
-                text.BBox.Right,
-                fields._Anchor0.BBox.Top
+            text.BBox.Left,
+            fields._Anchor2.BBox.Bottom,
+            text.BBox.Right,
+            text.BBox.Bottom
         )
         if text.Length > 0 then
-            fields.CustomerName = text[0]:ToString():gsub('^[%s%:]+', '')
+            local strSum = ''
+            for i = 0, text.Length-1 do
+                if text[i]:ToString():match('Due%s+Date') then
+                    break
+                end
+                strSum = strSum .. text[i]:ToString()
+                if i < text.Length-1 then
+                    strSum = strSum .. ' '
+                end
+            end
+            local customer, address = strSum:match('^(.+)%s+([A-Z][a-z]+%s+%d+%,?%s+%d%d%d%d+.+)$')
+            fields.CustomerName = customer or strSum
+            fields.CustomerAddress = address or strSum
         end
     end
-    if fields._InvoiceDate then
-        local dd, mon, yyyy = fields._InvoiceDate.Text:match('(%d+)%s*([%a%p]+)%s*(%d%d+)')
-        local months = { { ind = 'januari', eng = 'jan' }, { ind = 'februari', eng = 'feb' }, { ind = 'maret', eng = 'mar' }, { ind = 'april', eng = 'apr' }, { ind = 'mei', eng = 'may' }, { ind = 'juni', eng = 'jun' }, { ind = 'juli', eng = 'jul' }, { ind = 'agustus', eng = 'aug' }, { ind = 'september', eng = 'sep' }, { ind = 'oktober', eng = 'oct' }, { ind = 'november', eng = 'nov' }, { ind = 'desember', eng = 'dec' } }
-        for _, month in ipairs(months) do
-            if mon and string.lower(mon) == month['ind'] then
-                mon = month['eng']
-            end
-        end
-        if dd and mon and yyyy then
-            fields.InvoiceDate = ctx:CreateDate(dd .. mon .. yyyy, 'ddmonyyyy') or ctx:CreateDate(dd .. mon .. yyyy, 'ddmonyy') or nil
+    if fields._Anchor1 then
+        local text = ctx['text']
+        text = text:GetArea(
+                text.BBox.Left,
+                fields._Anchor1.BBox.Top,
+                text.BBox.Right,
+                fields._Anchor1.BBox.Bottom + text.AvgWordHeight
+        )
+        if text.Length > 0 then
+            total = CastToDecimal(text[0]:ToString(), ctx)
+            fields.InvoiceTotal = total or nil
         end
     end
-end
-function Finalize(fields, tables, ctx)
-    if tables.LineItems then
-        local extraTopLineIndex
-        local items = tables.LineItems
-        local strSum = ''
-        for i = items.Length - 1, 0, -1 do
-            if items[i].UnitPrice.Value then
-                items[i].UnitPrice = CastToDecimal(items[i].UnitPrice.Text, ctx)
+    if fields._Anchor0 then
+        local text = ctx['text']
+        text = text:GetArea(
+                text.BBox.Left,
+                fields._Anchor0.BBox.Top,
+                text.BBox.Right,
+                text.BBox.Bottom
+        )
+        if text.Length > 0 then
+            local strSum = ''
+            for i=0, text.Length-1 do
+                local line = text[i]:ToString()
+                if line:match('Amount') then
+                    break
+                end
+                strSum = strSum .. line
+                if i < text.Length-1 then
+                    strSum = strSum .. ' '
+                end
             end
-            if items[i].Total.Value then
-                items[i].Total = CastToDecimal(items[i].Total.Text, ctx)
-            end
-            if i > 0 and i < items.Length - 1 and not items[i].Description.Value then
-                local prev = items[i - 1].Description.Value and not items[i - 1].Total.Value and items[i - 1].Description.Text or ''
-                local next = items[i + 1].Description.Value and not items[i + 1].Total.Value and ' ' .. items[i + 1].Description.Text or ''
-                items[i].Description = prev .. next
-                extraTopLineIndex = i - 1
-                items:RemoveRow(i + 1)
-            end
-            if extraTopLineIndex and i == extraTopLineIndex then
-                extraTopLineIndex = nil
-                items:RemoveRow(i)
-            end
-        end
-        for i = items.Length - 1, 0, -1 do
-            if not items[i].Total.Value then
-                strSum = items[i].Description.Value and (items[i].Description.Text .. ' ' .. strSum) or strSum
-                items:RemoveRow(i)
-            else
-                items[i].Description = items[i].Description.Value and (items[i].Description.Text .. ' ' .. strSum) or strSum
-                strSum = ''
+            if string.len(strSum) > 0 then
+                description = strSum
             end
         end
-        if fields.Discount then
-            local decimal = CastToDecimal(fields.Discount.Text, ctx)
-            items:AppendRow()
-            items[-1].Description = 'Discount'
-            items[-1].Qty = 1
-            items[-1].UnitPrice = decimal and decimal * -1 or nil
-            items[-1].Total = decimal and decimal * -1 or nil
-            fields.Discount = nil
-        end
+    end
+    fields.OutputDescription = description
+    if description and total then
+        local items = ctx:CreateTable()
+        items:AppendColumn('Description')
+        items:AppendColumn('Total')
+        tables['LineItems'] = items
+        items:AppendRow()
+        items[-1].Description = description
+        items[-1].Total = total
     end
 end
--- src/sample7.lua
+++ src/sample7.lua
@@ -1,118 +1,12 @@
-function PostExtract(fields, tables, ctx)
-    if fields.CustomerCode then
-        local text = ctx['text']
-        local h = text.AvgWordHeight
-        local x0 = text.BBox.Left
-        local y0 = fields.CustomerCode.BBox.Top - h * 2
-        local x1 = text.BBox.Right
-        local y1 = fields.CustomerCode.BBox.Bottom - h * 1
-        text = text:GetArea(x0, y0, x1, y1)
-        if text.Length > 0 then
-            fields.CustomerName = text[0]:ToString()
+function Finalize(fields, tables, ctx)
+    if tables.LineItems then
+        local items = tables.LineItems
+        if items.Length > 0 then
+            local total = items[-1].Remarks.Value and items[-1].Remarks.Text:match('Total') and items[-1].Total.Value or nil
+            if total then
+                items:RemoveRow(items.Length - 1)
+                fields.InvoiceTotal = total
+            end
         end
     end
-    if fields._Anchor0 and fields._Anchor1 then
-        local text = ctx['text']
-        text = text:GetArea(text.BBox.Left, fields._Anchor0.BBox.Bottom, text.BBox.Right, fields._Anchor1.BBox.Top)
-        if text.Length > 0 then
-            local receiptIndex
-            local itemPrice
-            local itemQty
-            local itemUOM
-            local description = ''
-            local items = ctx:CreateTable()
-            items:AppendColumn('Description')
-            items:AppendColumn('Qty')
-            items:AppendColumn('UOM')
-            items:AppendColumn('UnitPrice')
-            items:AppendColumn('Total')
-            tables['LineItems'] = items
-            local s = ''
-            for i = 0, text.Length - 1 do
-                local line = text[i]:ToString()
-                if not receiptIndex and line:gsub('[%s%#]+', ''):match('^[0-9]+$') then
-                    receiptIndex = i
-                end
-                if receiptIndex and i > receiptIndex then
-                    local price, qty, uom = line:match('^([%d%,%.]+)[Xx%s]+([%d%,%.]+)%s+([A-Za-z%d]+)')
-                    local total = line:match('(%-?[%d%,%.]+)%s+A$')
-                    if price and qty and uom then
-                        itemPrice = price
-                        itemQty = qty
-                        itemUOM = uom
-                    end
-                    if not price and not qty and not uom and not line:match('Kvitas') then
-                        description = description .. ' ' .. line:gsub('%-?[%d%,%.]+%s+A$', '')
-                    end
-                    if total then
-                        items:AppendRow()
-                        items[-1].UOM = itemUOM or nil
-                        items[-1].UnitPrice = itemPrice and CastToDecimal(itemPrice, ctx) or nil
-                        items[-1].Qty = itemQty and CastToDecimal(itemQty, ctx) or nil
-                        items[-1].Description = description
-                        items[-1].Total = total and CastToDecimal(total, ctx) or nil
-                        description = ''
-                        itemQty = nil
-                        itemPrice = nil
-                        itemUOM = nil
-                    end
-                    if line:match('Nuolaida') then
-                        break
-                    end
-                end
-            end
-            fields.ItemSummary = s
-        end
-    end
-end
-function CastToDecimal(str, ctx)
-    local decimal = str:gsub('%,+', '.')
-    return ctx:CreateDecimal(decimal) or nil
-end
-function Finalize(fields, tables)
-    --[[ SplitSeriesNumber --]] if fields.SeriesNumber then
-        local delimiters = { 'nr', 'no', '-', ' ' }
-        str = string.lower(fields.SeriesNumber.Text)
-        str = string.gsub(str, '[.:,;<>#]+', '')
-        str = string.gsub(str, '^%s*(.-)%s*$', '%1')
-        local result = nil
-        local separator = nil
-        local series = ''
-        local number = ''
-        for i = 1, #delimiters do
-            if string.match(str, delimiters[i]) then
-                local t = {}
-                for str in string.gmatch(str .. delimiters[i], '(.-)' .. delimiters[i]) do
-                    t[#t + 1] = str
-                end
-                result = t
-                separator = delimiters[i]
-                break
-            end
-        end
-        if not result then
-            series, number = string.match(str, '(.-%a)(%d%d%d%d+)')
-            if not series and not number then
-                number = str
-            end
-        else
-            if not string.match(result[1], '%a') then
-                number = str
-            else
-                series = result[1]
-                for i = 2, #result do
-                    number = number == '' and number .. result[i] or number .. separator .. result[i]
-                end
-            end
-        end
-        series = series or ''
-        series = string.gsub(series, '%s+', '')
-        series = string.upper(series)
-        number = number or ''
-        number = string.gsub(number, '%s+', '')
-        number = string.upper(number)
-        fields.Series = series
-        fields.InvoiceNumber = number
-        fields.SeriesNumber = nil
-    end --[[ SplitSeriesNumber --]]
 end
